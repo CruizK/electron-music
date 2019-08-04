8aa0b1beb70047e14d5265138d17077b83d1ea24
CruizK <cruiz.kusman@gmail.com>
2019-08-03 23:16:12 -0500

    Fixed timeUtils formatSongDuration to not display '0:60'

--- client/src/utils/timeUtils.js
+++ client/src/utils/timeUtils.js
@@ -1,8 +1,9 @@
 
 
 export function formatSongDuration(time) {
+  time = Math.ceil(time);
   let minutes = Math.floor(time/60);
-  let seconds = Math.ceil(time % 60);
+  let seconds = time % 60;
   if(seconds < 10) {
     seconds = '0'+seconds;
   }
